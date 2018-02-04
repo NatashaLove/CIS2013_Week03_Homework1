ca0f726764a760fac03253ba6f39e1caa49a9b3f
Natasha S Love <debora@inbox.ru>
2018-02-04 16:23:37 -0600

    blackJack game
    started with black jack game program

---
+++ CIS2013_Week03_Homework1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+	int plcard_1, plcard_2, plcard_3;
+	int dlcard_1, dlcard_2, dlcard_3;
+	char question = 'y';
+	char hitStick;
