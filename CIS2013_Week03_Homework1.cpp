314f7b8cb9e17133ccbca7905a6313d970e59b9d
Natasha S Love <debora@inbox.ru>
2018-02-04 16:32:37 -0600

    2 random player cards

--- CIS2013_Week03_Homework1.cpp
+++ CIS2013_Week03_Homework1.cpp
@@ -9,3 +9,32 @@ int main() {
 	int dlcard_1, dlcard_2, dlcard_3;
 	char question = 'y';
 	char hitStick;
+
+	cout << "Welcome to Black Jack!";
+	cout << "Take 2 cards";
+
+	srand(time(NULL));
+
+	/* generate secret number between 1 and 10: */
+	int plcard_1 = rand() % 10 + 1;
+
+	cout << "your first card is " << plcard_1;
+
+	srand(time(NULL));
+
+	/* generate secret number between 1 and 10: */
+	int plcard_2 = rand() % 10 + 1;
+
+	cout << "your second card is " << plcard_2;
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
