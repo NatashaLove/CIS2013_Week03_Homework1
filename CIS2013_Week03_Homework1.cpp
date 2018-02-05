6f49bb3584658a92318686dfc7e3fddb9bde566f
Natasha S Love <debora@inbox.ru>
2018-02-04 19:54:57 -0600

    working

--- CIS2013_Week03_Homework1.cpp
+++ CIS2013_Week03_Homework1.cpp
@@ -5,28 +5,68 @@
 using namespace std;
 
 int main() {
-	
+	srand(time(NULL));
 	char question = 'y';
-	char hitStick;
+	
 
 	cout << "Welcome to Black Jack!"<< endl;
 	cout << "Take 2 cards" << endl;
+	cout << endl;
 
-
-	/* generate secret number between 1 and 10: */
+	/* generate number between 1 and 10: */
 	int plcard_1 = rand() % 10 + 1;
 
 	cout << "your first card is " << plcard_1 << endl;
-	
 
-
-	srand(time(NULL));
-
-	/* generate secret number between 1 and 10: */
+	/* generate number between 1 and 10: */
 	int plcard_2 = rand() % 10 + 1;
 
 	cout << "your second card is " << plcard_2 << endl;
 
+	/* generate number between 1 and 10: */
+	int dlcard_1 = rand() % 10 + 1;
+
+	cout << endl;
+	cout << "the dealer's card is " << dlcard_1 << endl;
+
+	cout << endl;
+
+	cout << "Do you want to hit more? y/n" << endl;
+	cin >> question;
+
+		if (question == 'y') {
+			int plcard_3 = rand() % 10 + 1;
+
+			cout << "your third card is " << plcard_3 << endl;
+
+			cout << " The total value of your hand is " << plcard_1 + plcard_2 + plcard_3 << endl;
+
+			if (plcard_1 + plcard_2 + plcard_3 > 21) {
+
+				cout << "You lose!" << endl;
+			}
+			else if (plcard_1 + plcard_2 + plcard_3 == 21) {
+
+				cout << "You win!" << endl;
+			}
+
+			else if (plcard_1 + plcard_2 + plcard_3 < 21) {
+				cout << "Do you want to hit more? y/n" << endl;
+				cin >> question;
+			}
+		
+	}
+	
+	
+		else {
+			/* generate number between 1 and 10: */
+			int dlcard_2 = rand() % 10 + 1;
+
+			cout << endl;
+			cout << "the dealer's second card is " << dlcard_2 << endl;
+		}
+	
+	cout << endl;
 
 
 
